06f97c222ce67651e227d74844f9f6d4dee72bf5
opilane <opilane@HP-CZC3217060>
2024-10-30 16:22:03 +0200

    Material Model WIP

---
+++ Models/MaterialModel.cs
@@ -0,0 +1,13 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Codie.Models
+{
+    [Table("Materials")]
+    public class MaterialModel
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
