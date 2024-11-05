0637979feeff2ef023eea0784f11b8f0bf508617
Timur Denisenko <timur.denisenko.2109@gmail.com>
2024-11-05 06:11:47 +0200

    buttons for next page and previous page

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -1,11 +1,43 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
 
 namespace Codie.Controllers
 {
     public class BookController : Controller
     {
-        public ActionResult Introduction () => View();
-        public ActionResult BasicTypes() => View();
-        public ActionResult Methods() => View();
+        public static string PreviosPage;
+        public static string NextPage;
+        private int CurrentNumPage;
+        private readonly Dictionary<int, string> pages = new Dictionary<int, string>()
+        {
+            {0, "404"},
+            {1, "Introduction"},
+            {2, "BasicTypes"},
+            {3, "Methods"},
+
+        };
+        private void pageOpened(int currentPage)
+        {
+            CurrentNumPage = currentPage;
+            PreviosPage = pages[pages.ContainsKey(currentPage - 1) ? currentPage - 1 : 0];
+            NextPage = pages[pages.ContainsKey(currentPage + 1) ? currentPage + 1 : 0];
+        }
+        public ActionResult Introduction()
+        {
+            pageOpened(1);
+            return View();
+        }
+
+        public ActionResult BasicTypes()
+        {
+            pageOpened(2);
+            return View();
+        }
+
+        public ActionResult Methods()
+        {
+            pageOpened(3);
+            return View();
+        }
     }
 }
