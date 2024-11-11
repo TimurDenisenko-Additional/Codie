4f22d9c75ee4d59783720f61e41cb5b501a2cd1d
Timur Denisenko <timur.denisenko.2109@gmail.com>
2024-11-11 08:41:26 +0200

    All materials for book

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -14,7 +14,18 @@ namespace Codie.Controllers
             {1, "Introduction"},
             {2, "BasicTypes"},
             {3, "Methods"},
-
+            {4, "OOP"},
+            {5, "Memory"},
+            {6, "ErrorHandling" },
+            {7, "Array"},
+            {8, "Lambda"},
+            {9, "FileManage" },
+            {10, "Async" },
+            {11, "WindowsForms" },
+            {12, "ADO" },
+            {13, "ASP" },
+            {14, "TDD" },
+            {15, "Threads" }
         };
         private void pageOpened(int currentPage)
         {
@@ -37,6 +48,78 @@ namespace Codie.Controllers
         public ActionResult Methods()
         {
             pageOpened(3);
+            return View();
+        }
+
+        public ActionResult OOP()
+        {
+            pageOpened(4);
+            return View();
+        }
+
+        public ActionResult Memory() 
+        { 
+            pageOpened(5);
+            return View();
+        }
+
+        public ActionResult ErrorHandling()
+        {
+            pageOpened(6);
+            return View();
+        }
+
+        public ActionResult Array()
+        {
+            pageOpened(7);
+            return View();
+        }
+
+        public ActionResult Lambda()
+        {
+            pageOpened(8);
+            return View();
+        }
+
+        public ActionResult FileManage()
+        {
+            pageOpened(9);
+            return View();
+        }
+
+        public ActionResult Async()
+        {
+            pageOpened(10);
+            return View();
+        }
+
+        public ActionResult WindowsForms()
+        {
+            pageOpened(11);
+            return View();
+        }
+
+        public ActionResult ADO()
+        {
+            pageOpened(12);
+            return View();
+        }
+
+        public ActionResult ASP()
+        {
+            pageOpened(13);
+            return View();
+        }
+
+        public ActionResult TDD()
+        {
+            pageOpened(14);
+            return View();
+        }
+
+        public ActionResult Threads()
+        {
+            pageOpened(15);
             return View();
         }
     }
