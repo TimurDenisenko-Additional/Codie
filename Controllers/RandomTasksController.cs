245a896f9eba9f066b5d69df739ba5806303872d
Timur Denisenko <timur.denisenko.2109@gmail.com>
2024-11-18 08:01:32 +0200

    HardTest

--- Controllers/RandomTasksController.cs
+++ Controllers/RandomTasksController.cs
@@ -26,18 +26,31 @@ namespace Codie.Controllers
             Tuple<string, Task[]> SingleTask = Tuple.Create(scm.Statement, JsonConvert.DeserializeObject<Task[]>(scm.Options));
             return SingleTask;
         }
+        private CodeTaskModel GetCodeTask() =>
+            db.CodeTaskModels.ToArray()[rndNum.Next(0, db.CodeTaskModels.Count())];
+
         public ActionResult EasyTest()
         {
             Tuple<string, Task[]> SingleTask = GetSingleTask();
             Tuple<string, Task[]> MultiTask = GetMultiTask();
-            CodeTaskModel ctm = db.CodeTaskModels.ToArray()[rndNum.Next(0, db.CodeTaskModels.Count())];
-            return View((SingleTask, MultiTask, ctm).ToTuple());
+            return View((SingleTask, MultiTask, GetCodeTask()).ToTuple());
         }
 
-        public ActionResult MediumTest()
+        private void FillMultiTasks(Tuple<string, Task[]>[] MultiTasks)
         {
-            Tuple<string, Task[]>[] SingleTasks = new Tuple<string, Task[]>[3];
-            Tuple<string, Task[]>[] MultiTasks = new Tuple<string, Task[]>[2];
+            byte arrayCount = 0;
+            while (arrayCount != MultiTasks.Length)
+            {
+                Tuple<string, Task[]> mcmTuple = GetMultiTask();
+                if (!MultiTasks.Contains(mcmTuple))
+                {
+                    MultiTasks[arrayCount++] = mcmTuple;
+                }
+            }
+        }
+
+        private void FillSingleTasks(Tuple<string, Task[]>[] SingleTasks)
+        {
             byte arrayCount = 0;
             while (arrayCount != SingleTasks.Length)
             {
@@ -47,17 +60,40 @@ namespace Codie.Controllers
                     SingleTasks[arrayCount++] = scmTuple;
                 }
             }
-            arrayCount = 0;
-            while (arrayCount != MultiTasks.Length)
-            {
-                Tuple<string, Task[]> mcmTuple = GetMultiTask();
-                if (!MultiTasks.Contains(mcmTuple))
-                {
-                    MultiTasks[arrayCount++] = mcmTuple;
-                }
-            }
+        }
+
+        public ActionResult MediumTest()
+        {
+            Tuple<string, Task[]>[] SingleTasks = new Tuple<string, Task[]>[3];
+            Tuple<string, Task[]>[] MultiTasks = new Tuple<string, Task[]>[2];
+            FillSingleTasks(SingleTasks);
+            FillMultiTasks(MultiTasks);
             CodeTaskModel ctm = db.CodeTaskModels.ToArray()[rndNum.Next(0, db.CodeTaskModels.Count())];
             return View((SingleTasks, MultiTasks, ctm).ToTuple());
+        }
+
+        private void FillCodeTasks(CodeTaskModel[] CodeTasks)
+        {
+            byte arrayCount = 0;
+            while (arrayCount != CodeTasks.Length)
+            {
+                CodeTaskModel CodeTask = GetCodeTask();
+                if (!CodeTasks.Contains(CodeTask))
+                {
+                    CodeTasks[arrayCount++] = CodeTask;
+                }
+            }
+        }
+
+        public ActionResult HardTest()
+        {
+            Tuple<string, Task[]>[] SingleTasks = new Tuple<string, Task[]>[7];
+            Tuple<string, Task[]>[] MultiTasks = new Tuple<string, Task[]>[5];
+            CodeTaskModel[] CodeTasks = new CodeTaskModel[3];
+            FillSingleTasks(SingleTasks);
+            FillMultiTasks(MultiTasks);
+            FillCodeTasks(CodeTasks);
+            return View((SingleTasks, MultiTasks, CodeTasks).ToTuple());
         }
     }
 }
