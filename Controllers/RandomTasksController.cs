d961bd329832d80b2165038d49e8aa18fc9ed735
Timur Denisenko <timur.denisenko.2109@gmail.com>
2024-11-18 07:35:23 +0200

    Medium random test

--- Controllers/RandomTasksController.cs
+++ Controllers/RandomTasksController.cs
@@ -13,14 +13,51 @@ namespace Codie.Controllers
     {
         DBContext db = new DBContext();
         Random rndNum = new Random();
-        public ActionResult EasyTest() 
-        { 
-            SingleChoiseModel scm = db.SingleChoiseModels.ToArray()[rndNum.Next(0, db.SingleChoiseModels.Count())];
-            Tuple<string, Task[]> SingleTask = Tuple.Create(scm.Statement, JsonConvert.DeserializeObject<Task[]>(scm.Options));
+        private Tuple<string, Task[]> GetMultiTask()
+        {
             MultiChoiseModel mcm = db.MultiChoiseModels.ToArray()[rndNum.Next(0, db.MultiChoiseModels.Count())];
             Tuple<string, Task[]> MultiTask = Tuple.Create(mcm.Statement, JsonConvert.DeserializeObject<Task[]>(mcm.Options));
+            return MultiTask;
+        }
+
+        private Tuple<string, Task[]> GetSingleTask()
+        {
+            SingleChoiseModel scm = db.SingleChoiseModels.ToArray()[rndNum.Next(0, db.SingleChoiseModels.Count())];
+            Tuple<string, Task[]> SingleTask = Tuple.Create(scm.Statement, JsonConvert.DeserializeObject<Task[]>(scm.Options));
+            return SingleTask;
+        }
+        public ActionResult EasyTest()
+        {
+            Tuple<string, Task[]> SingleTask = GetSingleTask();
+            Tuple<string, Task[]> MultiTask = GetMultiTask();
             CodeTaskModel ctm = db.CodeTaskModels.ToArray()[rndNum.Next(0, db.CodeTaskModels.Count())];
             return View((SingleTask, MultiTask, ctm).ToTuple());
+        }
+
+        public ActionResult MediumTest()
+        {
+            Tuple<string, Task[]>[] SingleTasks = new Tuple<string, Task[]>[3];
+            Tuple<string, Task[]>[] MultiTasks = new Tuple<string, Task[]>[2];
+            byte arrayCount = 0;
+            while (arrayCount != SingleTasks.Length)
+            {
+                Tuple<string, Task[]> scmTuple = GetSingleTask();
+                if (!SingleTasks.Contains(scmTuple))
+                {
+                    SingleTasks[arrayCount++] = scmTuple;
+                }
+            }
+            arrayCount = 0;
+            while (arrayCount != MultiTasks.Length)
+            {
+                Tuple<string, Task[]> mcmTuple = GetMultiTask();
+                if (!MultiTasks.Contains(mcmTuple))
+                {
+                    MultiTasks[arrayCount++] = mcmTuple;
+                }
+            }
+            CodeTaskModel ctm = db.CodeTaskModels.ToArray()[rndNum.Next(0, db.CodeTaskModels.Count())];
+            return View((SingleTasks, MultiTasks, ctm).ToTuple());
         }
     }
 }
